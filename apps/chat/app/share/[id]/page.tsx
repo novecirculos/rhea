9f8eae5e1f25ebc5ae4b291c4ea3a820f4995db4
Gabriel Rudy <gabrielrudy575@gmail.com>
2023-07-29 13:55:46 -0300

    fix(chat): remove opengraph image to run share screen on edge

--- apps/chat/app/share/[id]/page.tsx
+++ apps/chat/app/share/[id]/page.tsx
@@ -6,6 +6,8 @@ import { getSharedChat } from '@/app/actions'
 import { ChatList } from '@/components/chat-list'
 import { FooterText } from '@/components/footer'
 
+export const runtime = 'edge'
+
 export const preferredRegion = 'home'
 
 interface SharePageProps {
