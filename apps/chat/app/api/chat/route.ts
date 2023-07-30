3e2bf6f3a6c69e12d2aacadc7dff1c61ba6ef1ec
Gabriel Rudy <gabrielrudy575@gmail.com>
2023-07-30 18:22:08 -0300

    feature(chat/api): added ConversationalRetrievalQAChain to manage vector database

--- apps/chat/app/api/chat/route.ts
+++ apps/chat/app/api/chat/route.ts
@@ -1,14 +1,20 @@
 import { kv } from '@vercel/kv'
 import { StreamingTextResponse, LangChainStream, Message } from 'ai'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
-import { AIMessage, HumanMessage } from 'langchain/schema'
+
 import { auth } from '@/auth'
 import { nanoid } from '@/lib/utils'
+import { ChatGPTPluginRetriever } from 'langchain/retrievers/remote'
+import { ConversationalRetrievalQAChain } from 'langchain/chains'
+import { PromptTemplate } from 'langchain/prompts'
+import { getMemory } from '@/lib/langchain'
 
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
   const { messages } = await req.json()
+  const { memory, question } = getMemory(messages) // TODO: handle memory through a database like redis
+
   const userId = (await auth())?.user.id
 
   if (!userId) {
@@ -45,21 +51,68 @@ export async function POST(req: Request) {
     },
   })
 
-  const llm = new ChatOpenAI({
-    streaming: true,
+  // Customize the prompt to your needs
+  const template = `
+  You are a chatbot helping humans with their questions. Related to a fantasy universe called "Nove Círculos".
+  You should always answers on: pt-BR
+  
+  {context}
+  
+  Human: {question}
+  Assistant:
+  `
+
+  const prompt = new PromptTemplate({
+    template,
+    inputVariables: ['question', 'context'],
   })
 
-  llm
-    .call(
-      (messages as Message[]).map((m) =>
-        m.role == 'user'
-          ? new HumanMessage(m.content)
-          : new AIMessage(m.content),
-      ),
-      {},
-      [handlers],
-    )
-    .catch(console.error)
+  const streamingModel = new ChatOpenAI({
+    streaming: true,
+    modelName: 'gpt-3.5-turbo-16k',
+    callbacks: [handlers],
+  })
+
+  const nonStreamingModel = new ChatOpenAI({
+    modelName: 'gpt-4',
+  })
+
+  const retriever = new ChatGPTPluginRetriever({
+    url: 'https://biblioteca-espiral.fly.dev/query',
+    auth: {
+      bearer: process.env.PLUGIN_BEARER_TOKEN as string,
+    },
+    topK: 10,
+  })
+
+  const CONDENSE_QUESTION_TEMPLATE = `
+  Given the following conversation and a follow up input, if it is a question rephrase it to be a standalone question.
+  If it is not a question, just summarize the message.
+  
+  Chat history:
+  {chat_history}
+  Follow up input: {question}
+  Standalone input:
+  `
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    streamingModel,
+    retriever,
+    {
+      memory,
+      returnSourceDocuments: true,
+      qaChainOptions: {
+        type: 'stuff',
+        prompt: prompt,
+      },
+      questionGeneratorChainOptions: {
+        llm: nonStreamingModel,
+        template: CONDENSE_QUESTION_TEMPLATE,
+      },
+    },
+  )
+
+  chain.call({ question })
 
   return new StreamingTextResponse(stream)
 }
