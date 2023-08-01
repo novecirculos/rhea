b3d58cb203be58b8f593d51b167ac681376664c8
Gabriel Rudy <gabrielrudy575@gmail.com>
2023-08-01 18:37:06 -0300

    fix(chat): dark mode on empty screen

--- apps/chat/components/empty-screen.tsx
+++ apps/chat/components/empty-screen.tsx
@@ -22,14 +22,14 @@ const exampleMessages = [
 export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
   return (
     <div className="mx-auto max-w-2xl px-4">
-      <div className="rounded-lg border bg-white p-8 shadow">
-        <h1 className="mb-2 text-lg font-semibold">
+      <div className="rounded-lg border bg-white p-8 shadow dark:border-gray-950 dark:bg-gray-800">
+        <h1 className="mb-2 text-lg font-semibold dark:text-gray-50">
           Seja bem vindo(a) aos Nove Círculos
         </h1>
-        <p className="text-muted-foreground mb-2 leading-normal">
+        <p className="text-muted-foreground mb-2 leading-normal dark:text-gray-400">
           Esse é um espaço para criar e se imergir nesse universo de fantasia.
         </p>
-        <p className="text-muted-foreground leading-normal">
+        <p className="text-muted-foreground leading-normal  dark:text-gray-400">
           Você pode começar uma conversa com os seguintes tópicos:
         </p>
         <div className="mt-4 flex flex-col items-start space-y-2">
@@ -37,7 +37,7 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
             <Button
               key={index}
               variant="link"
-              className="text-primary h-auto p-0 text-base"
+              className="text-primary dark:text-secondary h-auto p-0 text-base"
               onClick={() => setInput(message.message)}
             >
               <IconArrowRight className="text-muted-foreground mr-2" />
