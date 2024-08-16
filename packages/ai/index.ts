f5f94952dd59bc90f6e06da495c8d1c44919e768
Gabriel Araújo <gabrielrudy575@gmail.com>
2024-08-15 21:26:16 -0300

    [#11, AI / Packages] - Initiate package using quave ai api
    - initial structure

---
+++ packages/ai/index.ts
@@ -0,0 +1,9 @@
+// let's use ky as wrapper of fetch
+// https://github.com/sindresorhus/ky
+import ky from 'ky'
+
+console.log('Hello @novecirculos/ai', process.env.QUAVE_API_KEY)
+
+const thread = () => {}
+
+export { thread }
