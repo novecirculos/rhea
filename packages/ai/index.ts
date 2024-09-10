58de4c5f2d9de38ec7955ccfb83f7125cf69048f
Gabriel Araújo <gabrielrudy575@gmail.com>
2024-09-10 10:18:58 -0300

    [#11] - AI Package
    adding thread() function to manage conversations

--- packages/ai/index.ts
+++ packages/ai/index.ts
@@ -1,9 +1,59 @@
-// let's use ky as wrapper of fetch
-// https://github.com/sindresorhus/ky
 import ky from 'ky'
 
-console.log('Hello @novecirculos/ai', process.env.QUAVE_API_KEY)
+const { QUAVE_API_KEY } = process.env
 
-const thread = () => {}
+const api = ky.create({
+  prefixUrl: 'https://ai.quave.dev/v1',
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        request.headers.set('x-api-key', QUAVE_API_KEY)
+      },
+    ],
+  },
+})
 
-export { thread }
+const messages = async ({ threadId }: { threadId: string }) => {
+  const res = await api.get(`thread/${threadId}`).json()
+
+  console.log(res)
+
+  return res
+}
+
+const thread = async ({
+  content,
+  creator,
+  threadId,
+}: {
+  content: string
+  creator: string
+  threadId?: string
+}) => {
+  const res: {
+    text: string
+    threadId: string
+    meta: {
+      tips: string[]
+      infos: string[]
+    }
+  } = await api
+    .post('thread', {
+      json: { content, creator, threadId },
+    })
+    .json()
+
+  return {
+    response: res.text,
+    threadId: res.threadId,
+  }
+}
+
+const { response, threadId } = await thread({
+  content: 'Hello',
+  creator: 'Quave',
+})
+
+console.log(response, threadId)
+
+export { thread, messages }
